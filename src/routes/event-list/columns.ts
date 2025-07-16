ac62f8f4ddc64f50ea99acca42daed0119b8b91a
ignatiusfairlight <lawaierang@gmail.com>
2025-07-16 20:27:07 +0800

    Add more sorting button to data table

--- src/routes/event-list/columns.ts
+++ src/routes/event-list/columns.ts
@@ -2,6 +2,10 @@ import type { ColumnDef } from "@tanstack/table-core";
 import { renderComponent } from "$lib/components/ui/data-table";
 import DataTableActions from "./data-table-actions.svelte";
 import DataTableTitleButton from "./data-table-title-button.svelte";
+import DataTableStartDateButton from "./data-table-start-date-button.svelte";
+import DataTableStartTimeButton from "./data-table-start-time-button.svelte";
+import DataTableEndDateButton from "./data-table-end-date-button.svelte";
+import DataTableEndTimeButton from "./data-table-end-time-button.svelte";
 
 export type Event = {
   id: string;
@@ -23,19 +27,31 @@ export const columns: ColumnDef<Event>[] = [
   },
   {
     accessorKey: "startDate",
-    header: "Start Date",
+    header: ({ column }) =>
+      renderComponent(DataTableStartDateButton, {
+        onclick: column.getToggleSortingHandler(),
+      }),
   },
   {
     accessorKey: "startTime",
-    header: "Start Time",
+    header: ({ column }) =>
+      renderComponent(DataTableStartTimeButton, {
+        onclick: column.getToggleSortingHandler(),
+      }),
   },
   {
     accessorKey: "endDate",
-    header: "End Date",
+    header: ({ column }) =>
+      renderComponent(DataTableEndDateButton, {
+        onclick: column.getToggleSortingHandler(),
+      }),
   },
   {
     accessorKey: "endTime",
-    header: "End Time",
+    header: ({ column }) =>
+      renderComponent(DataTableEndTimeButton, {
+        onclick: column.getToggleSortingHandler(),
+      }),
   },
   {
     accessorKey: "location",
